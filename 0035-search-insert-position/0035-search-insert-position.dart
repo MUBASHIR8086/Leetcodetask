eb3c7f9532f544d370dbb0701654365dd0d5798f
Mubashir T c <mubashirtc54@gmail.com>
2025-01-17 22:17:56 +0530

    Time: 0 ms (100%), Space: 152.5 MB (8.88%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.dart
+++ 0035-search-insert-position/0035-search-insert-position.dart
@@ -1,10 +1,14 @@
 class Solution {
-  int searchInsert(List<int> nums, int target) {
-    for(int n=0;n<nums.length;n++){
-        if(nums[n]==target||nums[n]>target){
-            return n;
-        }
+  int? searchInsert(List<int> nums, int target) {
+    int startidx=0;
+    int endidx = nums.length-1;
+    int middle=0;
+    while(startidx <= endidx){
+      middle=startidx+(endidx-startidx)~/2;
+      if(nums[middle]==target)return middle;
+      else if(nums[middle]>target)endidx=middle-1;
+      else startidx=middle+1;
     }
-    return nums.length;
+    if(nums[middle]>target)return middle;return middle+1;
   }
-}
+  }
