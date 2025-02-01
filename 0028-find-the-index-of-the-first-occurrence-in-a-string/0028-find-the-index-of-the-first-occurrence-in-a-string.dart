2a42d6502165900bb7b07bb1f0fddfbf47b92b3f
Mubashir T c <mubashirtc54@gmail.com>
2025-02-01 09:20:58 +0530

    Time: 0 ms (100%), Space: 146.8 MB (86.98%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.dart
@@ -0,0 +1,16 @@
+class Solution {
+  int strStr(String haystack, String needle) {
+    if(needle.isEmpty){
+        return 0;
+    }
+    int a=haystack.length;
+    int b=needle.length;
+    for(int i=0;i<=a-b;i++){
+         if (haystack.substring(i, i + b) == needle) {
+      return i;
+    }
+    }
+    return -1;
+    
+  }
+}
