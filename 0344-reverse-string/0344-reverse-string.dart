fdd320cbaa64f524fe4be7ad1faf71eef663c935
Mubashir T c <mubashirtc54@gmail.com>
2025-03-13 11:46:10 +0530

    Time: 1 ms (53.26%), Space: 149.9 MB (92.39%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.dart
+++ 0344-reverse-string/0344-reverse-string.dart
@@ -1,9 +1,9 @@
 class Solution {
   void reverseString(List<String> s) {
-    for (int left = 0, right = s.length - 1; left < right; left++, right--) {
-      String temp = s[left];
-      s[left] = s[right];
-      s[right] = temp;
+    for (int i = 0, n = s.length - 1; i < n; i++, n--) {
+      String temp = s[i];
+      s[i] = s[n];
+      s[n] = temp;
     }
   }
 }
