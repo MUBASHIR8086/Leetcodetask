5880a0a8362f916e2c22aa07697f8940decd0310
Mubashir T c <mubashirtc54@gmail.com>
2025-06-26 22:16:11 +0530

    Time: 1 ms (68.6%), Space: 150.4 MB (30.43%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.dart
+++ 0058-length-of-last-word/0058-length-of-last-word.dart
@@ -1,8 +1,8 @@
 class Solution {
-  int lengthOfLastWord(String s) {
-    List<String> splittedString = s.trim().split(' ');
-
-    return splittedString.last.length;
-  }
+ int lengthOfLastWord(String s) {
+  s = s.trim(); // Remove leading and trailing spaces
+  List<String> words = s.split(' ');
+  return words.last.length;
 }
 
+}
